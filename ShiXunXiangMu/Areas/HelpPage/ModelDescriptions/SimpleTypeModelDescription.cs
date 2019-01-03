7c2271cfddea415576a17e1d4e35e7c6d49e8116
家铮 华 <1518316576@qq.com>
2019-01-03 19:50:16 +0800

    正式提交

--- ShiXunXiangMu/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ShiXunXiangMu.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- ShiXunXiangMu/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShiXunXiangMu.WebApiApplication" Language="C#" %>
