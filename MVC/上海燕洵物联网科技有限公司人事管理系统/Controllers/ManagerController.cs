1e6f9fd9a73b5fa9827bd04524ec8276e8165714
2225030605@qq.com <2225030605@qq.com>
2019-01-04 11:30:17 +0800

    HttpClientHelpery以及经理的httpclienthelper删除

--- MVC/上海燕洵物联网科技有限公司人事管理系统/Controllers/ManagerController.cs
+++ MVC/上海燕洵物联网科技有限公司人事管理系统/Controllers/ManagerController.cs
@@ -206,45 +206,6 @@ namespace 上海燕洵物联网科技有限公司人事管理系统.Controllers
             return paymessage;
         }
     }
-    public class HttpClientHelper
-    {
-        public static string Seng(string method, string apiMethod, string JsonStr)
-        {
-            Uri uri = new Uri("http://localhost:51087/");
-            HttpClient client = new HttpClient();
-            client.BaseAddress = uri;
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage sponse = null;
-            switch (method)
-            {
-                case "get":
-                    sponse = client.GetAsync(apiMethod).Result;
-                    break;
-                case "post":
-                    HttpContent content = new StringContent(JsonStr);
-                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                    sponse = client.PostAsync(apiMethod, content).Result;
-                    break;
-                case "put":
-                    HttpContent content1 = new StringContent(JsonStr);
-                    content1.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                    sponse = client.PutAsync(apiMethod, content1).Result;
-                    break;
-                case "delete":
-                    sponse = client.DeleteAsync(apiMethod).Result;
-                    break;
-                default:
-                    break;
-
-            }
-            if (sponse.IsSuccessStatusCode)
-            {
-                return sponse.Content.ReadAsStringAsync().Result;
-            }
-            else
-            {
-                return "未知错误";
-            }
-        }
-    }
+   
+               
 }
