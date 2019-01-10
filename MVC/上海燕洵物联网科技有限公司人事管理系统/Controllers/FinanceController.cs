61193993fecfb76fa879b9b05915d701b7b26ab1
家铮 华 <1518316576@qq.com>
2019-01-10 14:33:39 +0800

    所有弹框修改

--- MVC/上海燕洵物联网科技有限公司人事管理系统/Controllers/FinanceController.cs
+++ MVC/上海燕洵物联网科技有限公司人事管理系统/Controllers/FinanceController.cs
@@ -57,7 +57,14 @@ namespace 上海燕洵物联网科技有限公司人事管理系统.Controllers
             vacate.EmpsId = Convert.ToInt32(Session["EmpsId"]);
             string str = JsonConvert.SerializeObject(vacate);
             string result = HttpClientHelper.Seng("post", "api/Finance/Vacatefinance",str);
-            return Content("<script>alert("+result+"),location.href='/login/Show'</script>");
+            if (result.Contains("成功"))
+            {
+                return Content("提交成功,等待审核");
+            }
+            else
+            {
+                return Content("提交失败,等待审核");
+            }
         }
 
         /// <summary>
