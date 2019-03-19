56fc1a314e3d6fffd4668e3e8dcd796ca70a0c37
家铮 华 <1518316576@qq.com>
2019-03-19 08:37:46 +0800

    1

--- API/DAL/Migrations/201901080540163_mymig.cs
+++ API/DAL/Migrations/201901080540163_mymig.cs
@@ -7,6 +7,7 @@ namespace DAL.Migrations
     {
         public override void Up()
         {
+            AlterColumn("dbo.")
         }
         
         public override void Down()
---
+++ MVC/上海燕洵物联网科技有限公司人事管理系统/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 上海燕洵物联网科技有限公司人事管理系统
+{
+    public class Class1
+    {
+    }
+}
